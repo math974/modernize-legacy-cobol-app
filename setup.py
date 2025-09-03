11ac2d9391972a937e36230587b95458d55acd79
math974 <mathias.ballot@epitech.eu>
2025-09-03 18:13:09 +0200

    fix pep8 norme

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
 import os
+
+from setuptools import find_packages, setup
+
 
 # Read the contents of README file
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
@@ -59,7 +61,7 @@ setup(
     keywords=[
         "cobol",
         "legacy",
-        "modernization", 
+        "modernization",
         "golden-master",
         "testing",
         "accounting",
